8a296c7e4971705c095ed4a16f3358ee6726f3f7
rmdobbins <russell.dobbins@yahoo.com>
2013-03-03 11:25:51 -0600

    initial setup

---
+++ src/root/test/main.py
@@ -0,0 +1,27 @@
+'''
+Created on Mar 3, 2013
+
+@author: rmdobbins
+'''
+import unittest
+import root.code.main
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.test = root.code.main
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        self.assertEqual("What is 1 + 1", self.test.generatePrompt())
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
